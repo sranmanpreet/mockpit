5f1aeefe0124d0d492a89b5bafdcc8f7bae28bf5
Sran Manpreet <manna1661@gmail.com>
2023-07-02 15:14:00 +0530

    Homepage template done

--- client/mockpit-ui/src/app/app.module.ts
+++ client/mockpit-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ShellComponent } from './components/shell/shell.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ShellComponent
+    ShellComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
