2e86ceeaf217c12f921639271404d6f7c55f5a5a
Sran Manpreet <manna1661@gmail.com>
2023-09-09 04:09:51 +0530

    Fixed entryPoint.sh syntax

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-envsubst < /usr/share/nginx/html/assets/config/config.template.json > /usr/share/nginx/html/assets/config/config.json
-nginx -g 'daemon off;' &
-java -jar /app/mockpit-server.jar
+#!/bin/sh
+envsubst < /usr/share/nginx/html/assets/config/config.template.json > /usr/share/nginx/html/assets/config/config.json && exec nginx -g 'daemon off;' & java -jar /app/mockpit-server.jar
